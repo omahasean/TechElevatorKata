13615ef661cb6f8446b090792698fa47481b6032
Sean Griswold <seangriswold@Seans-MacBook-Pro.local>
2020-05-29 10:25:14 -0400

    created studentscores service

--- src/views/Exercises.vue
+++ src/views/Exercises.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Exercises</h1>
+    <h1>Exercises Average Score {{ overallScore }}</h1>
     <card class="exercises">
       <exercises-list />
     </card>
@@ -10,9 +10,27 @@
 <script>
 import Card from '@/components/ui/Card';
 import ExercisesList from '@/components/ExercisesList';
+import StudentScoreService from '../services/StudentScoreService';
 
 export default {
   components: { Card, ExercisesList },
+  data() {
+    return {
+      scores: studentScores
+    }
+  }, 
+   async created() {
+    const scoreService = new StudentScoreService();
+    this.studentScores = await scoreService.getAll();
+  },
+  computed: {
+    overallScore() {
+      let sum = this.studentScores.reduce((currentSum, score) => {
+        return currentSum + this.studentScores.score;
+      },0);
+      return sum / this.studentScores.length;
+    }
+  }
 };
 </script>
 
